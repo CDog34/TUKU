5f31a01adf0e90fc52ccaebfa22045959792ae76
PeterCao <i@cdog.me>
2015-07-09 20:18:36 +0800

    回调认证

--- qbox/QBox.js
+++ qbox/QBox.js
@@ -1,13 +1,17 @@
 var qiniu = require("qiniu");
 var config=require("../conf.json");
+var crypto=require("crypto");
 qiniu.conf.ACCESS_KEY=config.accessKey;
 qiniu.conf.SECRET_KEY=config.secretKey;
 
 exports.genPutPolicy=function(filename,cbk){
+	console.log(config.siteAddress);
 	if (filename !="" ) {
 		filename=":"+filename;
 	}
 	var PP = new qiniu.rs.PutPolicy(config.bucketName+filename);
+	PP.callbackUrl=config.siteAddress+"a/cbk";
+	PP.callbackBody="name=$(x:friname)&hash=$(etag)&key=$(key)";
 	PP.returnBody= JSON.stringify({
 			"key" :"$(key)",
 			"size" : "$(fsize)",
@@ -19,5 +23,34 @@ exports.genPutPolicy=function(filename,cbk){
 	cbk(PP.token());
 	
 
+}
+
+function getRawBody(body){
+	var rst="";
+	var dot="";
+	for (key in body){
+		rst=rst+dot+encodeURIComponent(key)+"="+encodeURIComponent(body[key]);
+		dot="&";
+	}
+	return rst;
+}
+
+exports.isQiniuCallback=function(aStr,body,successCbk,failCbk){
+	if ( aStr == undefined || body == undefined ||aStr.substring(0,aStr.indexOf(" ")) != "QBox") {
+		failCbk();
+		return;
+	}
+	var params=aStr.substring(aStr.indexOf(" ")+1).split(":");
+	if (params.length != 2 || params[0] != config.accessKey){
+		failCbk();
+		return;
+	}
+	var data="/a/cbk\n"+getRawBody(body);
+	data=crypto.createHmac('sha1', config.secretKey).update(data).digest().toString("base64").replace(/\+/ig,"-").replace(/\//ig,"_");
+	if (data===params[1]){
+		successCbk();
+	}else{
+		failCbk();
+	}
 }
 
--- routes/ajax.js
+++ routes/ajax.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var qn = require('../qbox/QBox');
+var config = require('../conf.json');
 var router = express.Router();
 
 /* 生成上传凭证. */
@@ -7,6 +8,21 @@ router.get('/genPP', function(req, res) {
   qn.genPutPolicy(req.query.fname,function(token){
     	res.send(token);
 	});
+});
+
+router.post('/cbk',function(req,res){
+	console.log("收到回调请求");
+	qn.isQiniuCallback(req.header("Authorization"),req.body,function(){
+		console.log("合法请求");
+		res.json({"success":true,"url":config.siteAddress+"p/"+req.body.name});
+	},function(){
+		console.log("非法请求");
+		res.send("这不好玩！");
+	});
+	
+});
+router.get('/cbk',function(req,res){
+	res.send("这不好玩！");
 });
 
 module.exports = router;
