c448b425cd4c0436f77cd361ea247dac5e2dc6ce
CDog <i@cdog.me>
2015-11-27 14:12:48 +0800

    ggg

--- routes/debug.js
+++ routes/debug.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/upload', function(req, res) {
-  res.render("debug/upload",{title:"上传调试"});
-});
+//router.get('/upload', function(req, res) {
+//  res.render("debug/upload",{title:"上传调试"});
+//});
 
 module.exports = router;
