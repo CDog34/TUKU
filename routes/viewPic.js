ad06077ea5babf2a4b3a7c09a57f8a6ca7e8e99a
CDog <i@cdog.me>
2015-11-28 22:02:21 +0800

    ggg

--- routes/viewPic.js
+++ routes/viewPic.js
@@ -4,7 +4,7 @@ var conf=require("../conf.json");
 
 
 router.get('/*', function(req, res) {
-    res.redirect(conf.qnAddress+req.params[0]);
+    res.redirect(conf.qiniu.qnAddress+req.params[0]);
     //res.send("你请求的地址为："+req.params[0]);
 });
 
