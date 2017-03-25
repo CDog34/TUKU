2ee6d2971beecce6431661ac4c0fa192495b9df3
Cao Junquan <i@cdog.me>
2017-03-25 17:10:53 +0800

    Add Logger.

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 import Koa from 'koa';
 import config from './config';
+import {logStartUp, logInit, logForRequest} from './services/logger';
 
 const startServer = () => {
+  const startTime = Date.now();
   const app = new Koa();
 
+
+  app.use(logForRequest());
   app.use((ctx) => ctx.body = 'Hello World');
-  app.listen(config.port, () => console.info('Server Started'));
+  app.listen(config.port, logStartUp(startTime, config.env, config.port));
+
 };
 
+logInit();
 startServer();
---
+++ server/src/services/logger.js
@@ -0,0 +1,40 @@
+import winston from 'winston';
+import config from '../config';
+
+const logLevel = process.env.APP_LOGLEVEL || 'debug';
+const consoleTransportsOptions = {
+  timestamp() {
+    return (new Date()).toLocaleString();
+  },
+  colorize: true,
+  level: logLevel
+};
+let logger = null;
+
+export function logInit() {
+  logger = new winston.Logger({
+    transports: [
+      new (winston.transports.Console)(consoleTransportsOptions),
+    ]
+  });
+  logger.info('Log Level: %s', logLevel);
+  logger.info('Config env: %s', config.env);
+}
+
+export function logForRequest() {
+  return async(ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    const logMode = ctx.status <= 400 ? 'info' : 'warn';
+    logger[logMode]('%s %s - %sms %s %s', ctx.method, ctx.url, ms, ctx.status, ctx.headers['user-agent']);
+    ctx.set('X-Powered-By', `${config.appName}/${config.version}`);
+  };
+}
+
+export function logStartUp(startTime, env, port) {
+  const duration = Date.now() - startTime;
+  logger.info('Server running in %s mode.', env);
+  logger.info('Server startup in %sms.', duration);
+  logger.info('Waiting for client at %s.', port);
+}
