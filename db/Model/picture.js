c66dd3c1c6b4a6170d9b3e72b8d1bc2b148849b1
Cao Junquan <i@cdog.me>
2017-03-25 16:05:43 +0800

    Clean the repo.

--- db/Model/picture.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by CDog on 15/10/8.
- */
-var pictureSchema=require("../Schema/pictures");
-var mongoose=require("mongoose");
-
-var picture=mongoose.model("picture",pictureSchema);
-
-module.exports=picture;
--- routes/debug.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-//router.get('/upload', function(req, res) {
-//  res.render("debug/upload",{title:"上传调试"});
-//});
-
-module.exports = router;
