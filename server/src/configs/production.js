9572579d5b1b40e6e5bcb602e89d6e218a2d37c6
Cao Junquan <i@cdog.me>
2017-03-26 19:36:03 +0800

    Prepare for deploy.

--- server/src/configs/production.js
+++ server/src/configs/production.js
@@ -1,3 +1,27 @@
+
+
+const dbUrl = process.env.APP_DB_URL || '';
+import {logger} from '../services/loggerService';
+let secretConfig = null;
+
+try {
+  secretConfig = JSON.parse(process.env.PRODUCTION_SECRET);
+} catch (err) {
+  logger.error(err.message);
+  secretConfig = {};
+}
 export const envConfig = {
-  env: 'production'
+
+  env: 'production',
+
+  dbUrl: dbUrl,
+  CDNBase: '//tuku.izhai.net/',
+
+  upyun: {
+    uploadFilePrefix: '',
+    bucket: 'tuku-image-hosting',
+    endpoint: 'v0.api.upyun.com',
+    operator: secretConfig.operator,
+    password: secretConfig.password,
+  }
 };
--- server/src/configs/staging.js
+++ server/src/configs/staging.js
@@ -1,3 +1,26 @@
+const dbUrl = process.env.APP_DB_URL || '';
+import {logger} from '../services/loggerService';
+let secretConfig = null;
+
+try {
+  secretConfig = JSON.parse(process.env.DEVELOPMENT_SECRET);
+} catch (err) {
+  logger.error(err.message);
+  secretConfig = {};
+}
+
+
 export const envConfig = {
-  env: 'staging'
+  env: 'staging',
+
+  dbUrl: dbUrl,
+  CDNBase: 'http://tuku-2-staging.b0.upaiyun.com/',
+
+  upyun: {
+    uploadFilePrefix: 'stagingUpload',
+    bucket: 'tuku-2-staging',
+    endpoint: 'v0.api.upyun.com',
+    operator: secretConfig.operator,
+    password: secretConfig.password,
+  }
 };
