699c3aca38c73cae8386325fd2a5e0a5f7a3e1f9
Cao Junquan <i@cdog.me>
2017-03-28 12:41:48 +0800

    Fix ===.

--- server/src/controllers/uploadController.js
+++ server/src/controllers/uploadController.js
@@ -11,7 +11,7 @@ export async function handleImageUpload(image, userId) {
   const existImages = await Image.findByMd5sum(checkSum);
   let existImage = null;
   if (!!existImages && existImages.length) {
-    existImage = _.find(existImages, image => image.ownerId = userId) || {};
+    existImage = _.find(existImages, image => image.ownerId === userId) || {};
     if (existImage.ownerId === userId)
       return {
         image: existImage,
