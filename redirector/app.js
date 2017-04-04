bcfc413fcfcbc02aff7572af4257ff1d190ca0b2
Cao Junquan <i@cdog.me>
2017-04-04 21:37:34 +0800

    Add webp support to redirector.

--- redirector/app.js
+++ redirector/app.js
@@ -11,17 +11,19 @@ const movedPermanently = (res, location) => {
   res.end();
 };
 
-const getNewLocation = (urlArr, isHttps) => {
+const getNewLocation = (urlArr, isHttps, canWebp) => {
   const newUri = urlArr.slice(2).join('/');
-  return `http${isHttps ? 's' : ''}://dn-tatpics.qbox.me/${newUri}!normal`;
+  return `http${isHttps ? 's' : ''}://dn-tatpics.qbox.me/${newUri}!normal${canWebp ? '.webp' : ''}`;
 };
 
 const server = http.createServer((req, res) => {
   const urlArr = req.url.split('/');
   const referer = req.headers['referer'] || '';
   const isHttps = referer.indexOf('https://') === 0;
+  const accept = req.headers['accept'];
+  const webp = accept.indexOf('webp') !== -1;
   if (urlArr[1] !== 'v') return notFound(res);
-  return movedPermanently(res, getNewLocation(urlArr, isHttps));
+  return movedPermanently(res, getNewLocation(urlArr, isHttps, webp));
 });
 
 server.listen(2333, () => console.log('serverStart'));
