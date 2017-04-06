3d65021963cdb731560f2ace6e7bf15a887c2de9
Cao Junquan <i@cdog.me>
2017-04-06 12:57:58 +0800

    Fix relay Domain.

--- server/src/configs/base.js
+++ server/src/configs/base.js
@@ -4,7 +4,7 @@ export const baseConfig = {
   appName: pkg.name,
   version: pkg.version,
   port: 12530,
-  imageRelayDomain: ['localhost', '//tuku-api.izhai.net'],
+  imageRelayDomain: ['localhost', '//tuku.moe'],
   imageSizeLimit: 5 * 1024 * 1024,
 
   security: {
